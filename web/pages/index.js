620840c61529cd97377b2617b75c9bccac3f119f
05st <05st@users.noreply.github.com>
2021-08-29 00:16:42 -0500

    Set up CMS

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,7 +1,37 @@
-export default function Index() {
+import Link from "next/link";
+import client from "../client";
+
+function Index(props) {
+  const { posts = [] } = props;
   return (
-    <>
+    <div>
       <p>blog.stimsina.com</p>
-    </>
+      {posts.map(
+        ({ _id, title = '', slug = '', _updatedAt = '' }) =>
+          slug && (
+            <li key={_id}>
+              <Link href="/posts/[slug]" as={`/posts/${slug.current}`}>
+                <a>{title}</a>
+              </Link>{' '}
+              ({new Date(_updatedAt).toDateString()})
+            </li>
+          )
+      )}
+    </div>
   );
 }
+/*
+const query = `*[_type == "post" && publishedAt < now()]|order(publishedAt desc)`;
+
+Index.getInitialProps = async function() {
+  return await client.fetch(query);
+}
+*/
+
+Index.getInitialProps = async () => ({
+    posts: await client.fetch(`
+      *[_type == "post" && publishedAt < now()]|order(publishedAt desc)
+    `)
+})
+
+export default Index;
--- web/pages/posts/[slug].js
+++ web/pages/posts/[slug].js
@@ -1,10 +1,41 @@
-import { useRouter } from "next/router";
+import client from "../../client"
+import BlockContent from '@sanity/block-content-to-react'
 
-export default function Post() {
-  const router = useRouter();
+function Post(props) {
+  const {
+    title = "Missing title",
+    name = "Missing name",
+    categories,
+    body = []
+  } = props;
   return (
     <article>
-      <h1>{router.query.slug}</h1>
+      <h1>{title}</h1>
+      <span>By {name}</span>
+      {categories && (
+        <ul>
+          Posted in
+          {categories.map(category => <li key={category}>{category}</li>)}
+        </ul>
+      )}
+      <BlockContent
+        blocks={body}
+        {...client.config()}
+      />
     </article>
   );
 }
+
+const query = `*[_type == "post" && slug.current == $slug][0]{
+  title,
+  "name": author->name,
+  "categories": categories[]->title,
+  body
+}`;
+
+Post.getInitialProps = async function(ctx) {
+  const { slug = "" } = ctx.query;
+  return await client.fetch(query, { slug });
+}
+
+export default Post;
